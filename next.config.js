d80afbd11d2856bfeac9c10dedcde26bab9b8631
Bilal Hassan <bilal@smartcat.ca>
2022-11-19 11:29:42 -0500

    added sitemap and env file

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+    webpack: (config, { isServer }) => {
+      if (isServer) {
+        require('./scripts/generate-sitemap')
+      }
+  
+      return config
+    },
+  }
+  
